9a1719584c66052594037b24ced68b63d4bb9879
Alexey Minnekhanov <alexeymin@postmarketos.org>
2024-12-30 21:12:02 +0300

    clk/qcom: Port to new GDSC flags
    Now we need to specify flags for each GDSC for it to function properly.
    Copy .flags field from respective linux GCC driver.
    
    Turns out, even among SDM845 not all GDSCs are the same. Some of GDSCs
    do specify the POLL_CFG_GDSCR flag that changes the logic to check the
    ON/OFF status, other don't.
    
    Some qcom clock drivers in U-Boot don't specify any GDSCs at all
    (apq8016, apq8096, ipq4019, qcs404), or GDSCs in their linux GCC driver
    don't specify any .flags at all (qcm2290, sm6115) thus making them
    unaffected by this change.
    
    Add .flags to all other SoC's GDSCs currently present in u-Boot that do
    need this change.
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@postmarketos.org>

--- drivers/clk/qcom/clock-sc7280.c
+++ drivers/clk/qcom/clock-sc7280.c
@@ -98,8 +98,8 @@ static const struct qcom_reset_map sc7280_gcc_resets[] = {
 };
 
 static const struct qcom_power_map sc7280_gdscs[] = {
-	[GCC_UFS_PHY_GDSC] = { 0x77004 },
-	[GCC_USB30_PRIM_GDSC] = { 0xf004 },
+	[GCC_UFS_PHY_GDSC] = { 0x77004, .flags = VOTABLE },
+	[GCC_USB30_PRIM_GDSC] = { 0xf004, .flags = VOTABLE },
 };
 
 static struct msm_clk_data qcs404_gcc_data = {

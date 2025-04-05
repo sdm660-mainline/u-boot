f540e27181ef9572c11c5eb26c5c4bd38ef6ce41
Alexey Minnekhanov <alexeymin@postmarketos.org>
2025-04-05 18:17:22 +0300

    gpio: qcom_spmi: Add pm660 compatible to the new driver
    Allow to use PMIC GPIOs on SDM630/660 platforms.
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@postmarketos.org>

--- drivers/gpio/qcom_spmi_gpio.c
+++ drivers/gpio/qcom_spmi_gpio.c
@@ -745,6 +745,7 @@ static int qcom_spmi_pmic_gpio_probe(struct udevice *dev)
 static const struct udevice_id qcom_spmi_pmic_gpio_ids[] = {
 	{ .compatible = "qcom,pm6150l-gpio" },
 	{ .compatible = "qcom,pm6350-gpio" },
+	{ .compatible = "qcom,pm660-gpio" },
 	{ .compatible = "qcom,pm660l-gpio" },
 	{ .compatible = "qcom,pm7325-gpio" },
 	{ .compatible = "qcom,pm8550-gpio" },
